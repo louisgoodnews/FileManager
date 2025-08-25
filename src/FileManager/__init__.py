8d9a43a05d172fc5fa02165d0cf95aeb06bd9875
Louis Goodnews <louisgoodnews95@gmail.com>
2025-08-25 09:33:08 +0200

    Updated a few things.

---
+++ src/FileManager/__init__.py
@@ -0,0 +1,16 @@
+"""
+Author: Louis Goodnews
+Date: 2025-08-25
+"""
+
+from typing import Final, List, Literal
+
+from core.core import FileManager, FileTask
+
+
+__all__: Final[List[str]] = [
+    "FileManager",
+    "FileTask",
+]
+
+__version__: Final[Literal["0.1.0"]] = "0.1.0"
